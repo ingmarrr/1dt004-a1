325af717ceb368646dcad45fec5cbe3d70a3d9ac
Ingmar Falk <if222gq@student.lnu.se>
2024-11-15 14:03:10 +0100

    feat: part 2, finished

--- src/cyclic_wd.adb
+++ src/cyclic_wd.adb
@@ -1,4 +1,4 @@
---Cyclic scheduler with a watchdog: 
+--Cyclic scheduler with a watchdog:
 with Ada.Numerics.Float_Random;
 with Ada.Calendar;
 with Ada.Text_IO;
@@ -18,34 +18,16 @@ procedure cyclic_wd is
     Delta_F1   : constant Duration := 1.0;
     Delta_F3   : constant Duration := 2.0;
 
-
-    procedure f1 is
-    begin
-        Put_Line("f1 executing, time is " & Duration'Image(Clock - Start_Time));
-    end f1;
-
-    procedure f2 is
-    begin
-        Put_Line("f2 executing, time is " & Duration'Image(Clock - Start_Time));
-    end f2;
-
-    procedure f3 is
-        Random_Delay : Duration;
-    begin
-        Put_Line("f3 executing, time is " & Duration'Image(Clock - Start_Time));
-        Random_Delay := Duration(1.0 * Ada.Numerics.Float_Random.Random(Gen));
-        delay Random_Delay;
-        Put_Line("f3 completed, time is " & Duration'Image(Clock - Start_Time));
-        Scheduler.Stop;
-    end f3;
-
     protected Scheduler is
         procedure Start;
         procedure Stop;
         procedure Sync;
         function Deadline_Missed return Boolean;
+        function Warning_Sent return Boolean;
+        procedure Set_Warned_User(Value: Boolean);
     private
         Done            : Boolean;
+        Warned_User     : Boolean;
         Start_Time      : Time;
     end Scheduler;
 
@@ -71,17 +53,9 @@ procedure cyclic_wd is
                 exit when Done;
             end loop;
 
-            Current         := Clock;
-            Seconds_Part    := Integer(Seconds(Current));
-            Next_Second     := Current;
-            Next_Even       := Next_Second;
-
-            if Seconds_Part mod 2 /= 0 then
-                Next_Even := Next_Even + 1.0;
-            end if;
-
-            Next_F1 := Next_Second;
-            Next_F3 := Next_Even + 0.5;
+            Next_F1 := Next_F1 + Delta_F1;
+            Next_F3 := Next_F3 + Delta_F3;
+            Scheduler.Set_Warned_User(False);
         end Sync;
 
         function Deadline_Missed return Boolean is
@@ -89,16 +63,47 @@ procedure cyclic_wd is
             return Done and then (Clock - Start_Time) > Max_Duration;
         end Deadline_Missed;
 
+        function Warning_Sent return Boolean is
+        begin
+            return Warned_User;
+        end Warning_Sent;
+
+        procedure Set_Warned_User(Value: Boolean) is
+        begin
+            Warned_User := Value;
+        end Set_Warned_User;
+
     end Scheduler;
 
+
+    procedure f1 is
+    begin
+        Put_Line("f1 executing, time is " & Duration'Image(Clock - Start_Time));
+    end f1;
+
+    procedure f2 is
+    begin
+        Put_Line("f2 executing, time is " & Duration'Image(Clock - Start_Time));
+    end f2;
+
+    procedure f3 is
+        Random_Delay : Duration;
+    begin
+        Put_Line("f3 executing, time is " & Duration'Image(Clock - Start_Time));
+        Random_Delay := Duration(1.0 * Ada.Numerics.Float_Random.Random(Gen));
+        delay Random_Delay;
+        -- Put_Line("f3 completed, time is " & Duration'Image(Clock - Start_Time));
+        Scheduler.Stop;
+    end f3;
     task Watchdog;
 
     task body Watchdog is
     begin
         loop
             delay 0.1;
-            if Scheduler.Deadline_Missed then
+            if Scheduler.Deadline_Missed and not Scheduler.Warning_Sent then
                 Put_Line("[warning] F3 missed deadline");
+                Scheduler.Set_Warned_User(True);
             end if;
         end loop;
     end Watchdog;
@@ -115,16 +120,17 @@ begin
         delay until Next_F1 + Max_Duration;
         Next_F1 := Next_F1 + Delta_F1;
 
-        --  if Clock >= Next_F3 then
+         if Clock >= Next_F3 then
             Scheduler.Start;
             f3;
             if Scheduler.Deadline_Missed then
+                -- Put_Line("[info] syncing");
                 Scheduler.Sync;
             else
+                -- Put_Line("[info] updating Next_F3, is: " & Duration'Image(Clock - (Next_F3 + Delta_F3)));
                 Next_F3 := Next_F3 + Delta_F3;
             end if;
         end if;
 
     end loop;
 end cyclic_wd;
->>>>>>> 055c3f5 (feat: part 2, start)
