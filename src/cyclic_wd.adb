6d457ab17e1ca4895ff1b5d264cc96034b671962
Ingmar Falk <if222gq@student.lnu.se>
2024-11-15 13:31:53 +0100

    merged

--- src/cyclic_wd.adb
+++ src/cyclic_wd.adb
@@ -1,61 +1,130 @@
 --Cyclic scheduler with a watchdog: 
-
+with Ada.Numerics.Float_Random;
 with Ada.Calendar;
 with Ada.Text_IO;
 
 use Ada.Calendar;
 use Ada.Text_IO;
 
--- add packages to use randam number generator
-
-
 procedure cyclic_wd is
-    Message: constant String := "Cyclic scheduler with watchdog";
-        -- change/add your declarations here
-        d: Duration := 1.0;
-	Start_Time: Time := Clock;
-        
+    Message     : constant String := "Cyclic scheduler with watchdog";
+    Start_Time : Time := Clock;
+    Gen        : Ada.Numerics.Float_Random.Generator;
+    Max_Duration    : constant Duration := 0.5;
 
-	procedure f1 is 
-		Message: constant String := "f1 executing, time is now";
-	begin
-		Put(Message);
-		Put_Line(Duration'Image(Clock - Start_Time));
-	end f1;
+    Next_F1    : Time := Start_Time;
+    Next_F3    : Time := Start_Time + Max_Duration;
 
-	procedure f2 is 
-		Message: constant String := "f2 executing, time is now";
-	begin
-		Put(Message);
-		Put_Line(Duration'Image(Clock - Start_Time));
-	end f2;
+    Delta_F1   : constant Duration := 1.0;
+    Delta_F3   : constant Duration := 2.0;
 
-	procedure f3 is 
-		Message: constant String := "f3 executing, time is now";
-	begin
-		Put(Message);
-		Put_Line(Duration'Image(Clock - Start_Time));
-		-- add a random delay here
-	end f3;
-	
-	task Watchdog is
-	       -- add your task entries for communication 	
-	end Watchdog;
 
-	task body Watchdog is
-		begin
-		loop
-                 -- add your task code inside this loop    
-		end loop;
-	end Watchdog;
+    procedure f1 is
+    begin
+        Put_Line("f1 executing, time is " & Duration'Image(Clock - Start_Time));
+    end f1;
 
-	begin
+    procedure f2 is
+    begin
+        Put_Line("f2 executing, time is " & Duration'Image(Clock - Start_Time));
+    end f2;
 
+    procedure f3 is
+        Random_Delay : Duration;
+    begin
+        Put_Line("f3 executing, time is " & Duration'Image(Clock - Start_Time));
+        Random_Delay := Duration(1.0 * Ada.Numerics.Float_Random.Random(Gen));
+        delay Random_Delay;
+        Put_Line("f3 completed, time is " & Duration'Image(Clock - Start_Time));
+        Scheduler.Stop;
+    end f3;
+
+    protected Scheduler is
+        procedure Start;
+        procedure Stop;
+        procedure Sync;
+        function Deadline_Missed return Boolean;
+    private
+        Done            : Boolean;
+        Start_Time      : Time;
+    end Scheduler;
+
+    protected body Scheduler is
+        procedure Start is
+        begin
+            Done := False;
+            Start_Time := Clock;
+        end Start;
+
+        procedure Stop is
+        begin
+            Done := True;
+        end Stop;
+
+        procedure Sync is
+            Current         : Time;
+            Seconds_Part    : Integer;
+            Next_Second     : Time;
+            Next_Even       : Time;
+        begin
+            loop
+                exit when Done;
+            end loop;
+
+            Current         := Clock;
+            Seconds_Part    := Integer(Seconds(Current));
+            Next_Second     := Current;
+            Next_Even       := Next_Second;
+
+            if Seconds_Part mod 2 /= 0 then
+                Next_Even := Next_Even + 1.0;
+            end if;
+
+            Next_F1 := Next_Second;
+            Next_F3 := Next_Even + 0.5;
+        end Sync;
+
+        function Deadline_Missed return Boolean is
+        begin
+            return Done and then (Clock - Start_Time) > Max_Duration;
+        end Deadline_Missed;
+
+    end Scheduler;
+
+    task Watchdog;
+
+    task body Watchdog is
+    begin
         loop
-            -- change/add your code inside this loop     
-              f1;
-              f2;
-              f3;        
+            delay 0.1;
+            if Scheduler.Deadline_Missed then
+                Put_Line("[warning] F3 missed deadline");
+            end if;
         end loop;
-end cyclic_wd;
+    end Watchdog;
 
+begin
+    Ada.Numerics.Float_Random.Reset(Gen);
+    Put_Line(Message);
+
+    loop
+        delay until Next_F1;
+        f1;
+        f2;
+
+        delay until Next_F1 + Max_Duration;
+        Next_F1 := Next_F1 + Delta_F1;
+
+        --  if Clock >= Next_F3 then
+            Scheduler.Start;
+            f3;
+            if Scheduler.Deadline_Missed then
+                Scheduler.Sync;
+            else
+                Next_F3 := Next_F3 + Delta_F3;
+            end if;
+        end if;
+
+    end loop;
+end cyclic_wd;
+>>>>>>> 055c3f5 (feat: part 2, start)
