d11ced6c4028c0047de098ba8615549f9f15f932
mse-kbk <emese.kubik01@gmail.com>
2024-11-20 14:55:34 +0100

    finalised

--- src/comm1.adb
+++ src/comm1.adb
@@ -2,154 +2,145 @@
 
 with Ada.Calendar;
 with Ada.Text_IO;
-with Ada.Numerics.Discrete_Random;
 with Ada.Numerics.Float_Random;
+with Ada.Numerics.Discrete_Random;
 with Ada.Containers.Vectors;
 
+use type Ada.Containers.Count_Type;
+use Ada.Numerics.Float_Random;
 use Ada.Calendar;
 use Ada.Text_IO;
 
 procedure comm1 is
-    subtype Produced_Integer is Integer range 0..20;
-    package Integer_Vectors is new Ada.Containers.Vectors
-         (Index_Type   => Natural,
-          Element_Type => Produced_Integer);
-
-    use Integer_Vectors;
-    use type Ada.Containers.Count_Type;
-
-    package Random_Discrete is new Ada.Numerics.Discrete_Random(Produced_Integer);
-
     Message: constant String := "Process communication";
+	package Integer_Vectors is 
+				new Ada.Containers.Vectors (Index_Type => Positive, Element_Type => Integer);
+	subtype Vector is Integer_Vectors.Vector;
+
+	subtype PInteger is Integer range 0..20;
+	package D_Rand is new Ada.Numerics.Discrete_Random(PInteger);
 
 	task buffer is
-	    entry Can_Push(Value: out Boolean);
-        entry Push(Value: in Produced_Integer);
-        entry Can_Pop(Value: out Boolean);
-        entry Pop(Value: out Produced_Integer);
-        entry Stop;
+            -- add your task entries for communication 
+		entry Insert (i: in PInteger);
+		entry Remove (i: out PInteger);
+		entry Producer_Turn(Producer_Flag: out Boolean);
+		entry Consumer_Turn(Consumer_Flag: out Boolean);
+		entry stop;
 	end buffer;
 
 	task producer is
-        entry Start;
-        entry Stop;
+			entry stop;
 	end producer;
 
 	task consumer is
-        -- Nothing Needed, consumer is controlling the other tasks
+            -- add your task entries for communication 
 	end consumer;
 
-	task body buffer is
-		Message     : constant String  := "buffer executing";
-        Internal    : Integer_Vectors.Vector;
+task body buffer is
+	  Producer_Flag : Boolean := False;
+	  Consumer_Flag : Boolean := False;
+	  Shared_Buffer : Vector := Integer_Vectors.Empty_Vector;
+   begin
+      loop
+         select
+			accept Producer_Turn(Producer_Flag: out Boolean) do
+				Producer_Flag := (Shared_Buffer.Length) < 11;
+			end Producer_Turn;
+		or
+			accept Consumer_Turn(Consumer_Flag: out Boolean) do
+				Consumer_Flag := (Shared_Buffer.Length) > 0;
+			end Consumer_Turn;
+		or 
+			when (Shared_Buffer.Length) < 11 =>
+               accept Insert (I : in PInteger) do
+                  Shared_Buffer.Append (I);
+                  Producer_Flag := False;
+               end Insert;
+		or 
+			when (Shared_Buffer.Length) > 0 =>
+               accept Remove (I : out PInteger) do
+                  I := Shared_Buffer.First_Element;
+                  Shared_Buffer.Delete_First;
+                  Consumer_Flag := False;
+               end Remove;
+		or
+			accept stop;
+			exit;
+		end select;
+      end loop;
+   end buffer;
+
+	task body producer is 
+		Message: constant String := "producer executing";
+		D_Gen: D_Rand.Generator;
+		F_Gen: Ada.Numerics.Float_Random.Generator;
+		Value : PInteger;
+        Delay_Time : Duration;
+		Producer_Flag: Boolean := False;
 	begin
 		Put_Line(Message);
+		Ada.Numerics.Float_Random.Reset(F_Gen);
+		D_Rand.Reset(D_Gen);
 		loop
-            select
-                accept Can_Push(Value: out Boolean) do
-                    Value := Internal.Length < 10;
-                end Can_Push;
-            or
-                accept Can_Pop(Value: out Boolean) do
-                    Value := Internal.Length > 0;
-                end Can_Pop;
-            or
-                when Internal.Length < 10 =>
-                    accept Push(Value: in Produced_Integer) do
-                        Internal.Append(Value);
-                    end Push;
-            or
-                when Internal.Length > 0 =>
-                    accept Pop(Value: out Produced_Integer) do
-                        Value := Internal.First_Element;
-                        Internal.Delete_First;
-                    end Pop;
-            or
-                accept Stop;
-                exit;
-            or
-                terminate;
-            end select;
-		end loop;
-	end buffer;
-
-	task body producer is
-		Message       : constant String := "producer executing";
-		Float_Gen     : Ada.Numerics.Float_Random.Generator;
-		Discrete_Gen  : Random_Discrete.Generator;
-		Random_Delay  : Duration;
-		Random_Int    : Produced_Integer;
-		Can_Push      : Boolean := False;
-	begin
-		Put_Line(Message);
-		Random_Discrete.Reset(Discrete_Gen);
-		Ada.Numerics.Float_Random.Reset(Float_Gen);
-		accept Start;
-
-		loop
-            select
-                accept Stop;
-                exit;
-            else
-                Random_Delay := Duration(0.2 * Ada.Numerics.Float_Random.Random(Float_Gen));
-     			delay Random_Delay;
-
-     			Random_Int := Random_Discrete.Random(Discrete_Gen);
-     			loop
-     			    buffer.Can_Push(Can_Push);
-     			    exit when Can_Push;
-     			end loop;
-     			buffer.Push(Random_Int);
-            end select;
+		select
+			accept stop;
+			exit;
+		else
+			Delay_Time := Duration(0.5 * Ada.Numerics.Float_Random.Random(F_Gen));
+			Value:= D_Rand.Random(D_Gen);
+			delay Delay_Time;
+			loop 
+				buffer.Producer_Turn(Producer_Flag);
+				exit when Producer_Flag;
+			end loop;
+			buffer.Insert(Value);
+		end select;
 		end loop;
 	end producer;
 
-	task body consumer is
-		Message       : constant String := "consumer executing";
-		Float_Gen     : Ada.Numerics.Float_Random.Generator;
-		Random_Delay  : Duration;
-		Can_Pop       : Boolean := False;
-		Sum           : Integer := 0;
-		Popped        : Produced_Integer;
+	task body consumer is 
+		Message: constant String := "consumer executing";
+		Consumer_Flag: Boolean := False;
+		Sum : Integer := 0;
+        F_Gen: Ada.Numerics.Float_Random.Generator;
+		Value : PInteger;
+        Delay_Time : Duration;
 	begin
-	    Ada.Numerics.Float_Random.Reset(Float_Gen);
-		Put_Line(Message);
-		producer.Start;
+		Ada.Numerics.Float_Random.Reset(F_Gen);
 
-		Main_Loop:
+		Main_Cycle:
+		loop 
+		Delay_Time:= Duration(1.5 * Ada.Numerics.Float_Random.Random(F_Gen));
+		delay Delay_Time;
+
+		loop 
+			buffer.Consumer_Turn(Consumer_Flag);
+			exit when Consumer_Flag;
+		end loop;
+
 		loop
-            Random_Delay := Duration(2.0 * Ada.Numerics.Float_Random.Random(Float_Gen));
-            delay Random_Delay;
+			buffer.Consumer_Turn(Consumer_Flag);
+			buffer.Remove (Value);
+			Sum := Sum + Value;
 
-		    loop
-				buffer.Can_Pop(Can_Pop);
-				exit when Can_Pop;
-				delay 0.1;
-			end loop;
+			--Put_Line ("Consumed: " & Integer'Image(Value));
+			--Put_Line ("Sum: " & Integer'Image(Sum));
 
-			loop
-			    buffer.Can_Pop(Can_Pop);
-				exit when not Can_Pop;
-
-				buffer.Pop(Popped);
-				Sum := Sum + Popped;
-
-				if Sum > 100 then
-				    Put_Line("[Success] Consumer: Sum = " & Integer'Image(Sum));
-				    exit Main_Loop;
-				end if;
-			end loop;
-
-			Put_Line("[Info] Consumer: Sum = " & Integer'Image(Sum));
-			Sum := 0;
-
-		end loop Main_Loop;
-
-		producer.Stop;
-		buffer.Stop;
-
-	exception
-		  when TASKING_ERROR => Put_Line("Buffer finished before producer");
+			if Sum > 100 then
+				Put_Line ("Consumer: Reached termination condition. Sum = " & Integer'Image(Sum));
+               	exit Main_Cycle;
+            end if;
+			exit when not Consumer_Flag;
+        end loop;
+      end loop Main_Cycle;
+		
+	  producer.stop;
+	  buffer.stop;
+    
+		exception
+			  when TASKING_ERROR =>
+				  Put_Line("Buffer finished before producer");
 		Put_Line("Ending the consumer");
 	end consumer;
 begin
--- src/comm2.adb
+++ src/comm2.adb
@@ -1,57 +1,155 @@
---Protected types: Ada lab part 4
+--Process commnication: Ada lab part 3
 
 with Ada.Calendar;
 with Ada.Text_IO;
+with Ada.Numerics.Float_Random;
 with Ada.Numerics.Discrete_Random;
+with Ada.Containers.Vectors;
+
+use type Ada.Containers.Count_Type;
+use Ada.Numerics.Float_Random;
 use Ada.Calendar;
 use Ada.Text_IO;
 
 procedure comm2 is
-    Message: constant String := "Protected Object";
-    	type BufferArray is array (0 .. 9) of Integer;
-        -- protected object declaration
-	protected  buffer is
-            -- add entries of protected object here 
-	private
-            -- add local declarations
+    Message: constant String := "Protected object";
+	package Integer_Vectors is 
+				new Ada.Containers.Vectors (Index_Type => Positive, Element_Type => Integer);
+	subtype Vector is Integer_Vectors.Vector;
+
+	subtype PInteger is Integer range 0..20;
+	package D_Rand is new Ada.Numerics.Discrete_Random(PInteger);
+
+protected buffer is
+            -- add your task entries for communication 
+		entry Insert (i: in PInteger);
+		entry Remove (i: out PInteger);
+		procedure Producer_Turn(Producer_Flag: out Boolean);
+		procedure Consumer_Turn(Consumer_Flag: out Boolean);
+		procedure stop;
+        function Is_Stopped return Boolean;
+    private
+	  Stopped: Boolean:= False;
+	  Shared_Buffer : Vector := Integer_Vectors.Empty_Vector;
 	end buffer;
 
 	task producer is
-		-- add task entries
+			entry stop;
 	end producer;
 
 	task consumer is
-                -- add task entries
+            -- add your task entries for communication 
 	end consumer;
 
-	protected body buffer is 
-              -- add definitions of protected entries here 
-	end buffer;
+protected body buffer is
+    procedure Producer_Turn(Producer_Flag: out Boolean) is
+    begin
+			Producer_Flag := (Shared_Buffer.Length) < 11;
+	end Producer_Turn;
 
-        task body producer is 
+    procedure Consumer_Turn(Consumer_Flag: out Boolean) is
+    begin
+			Consumer_Flag := (Shared_Buffer.Length) > 0;
+	end Consumer_Turn;
+
+    entry Insert (i: in PInteger)
+        when (Shared_Buffer.Length) < 11 and not Stopped is
+    begin
+        Shared_Buffer.Append (i);
+    end Insert;
+
+	entry Remove (i: out PInteger)
+        when (Shared_Buffer.Length) >0 and not Stopped is
+    begin
+        i := Shared_Buffer.First_Element;
+        Shared_Buffer.Delete_First;
+    end Remove;
+
+    function Is_Stopped return Boolean is
+        begin
+            return Stopped;
+        end Is_Stopped;
+
+    procedure Stop is
+        begin
+            Stopped := True;
+        end Stop;
+   end buffer;
+
+	task body producer is 
 		Message: constant String := "producer executing";
-                -- add local declrations of task here  
+		D_Gen: D_Rand.Generator;
+		F_Gen: Ada.Numerics.Float_Random.Generator;
+		Value : PInteger;
+        Delay_Time : Duration;
+		Producer_Flag: Boolean := False;
 	begin
 		Put_Line(Message);
+		Ada.Numerics.Float_Random.Reset(F_Gen);
+		D_Rand.Reset(D_Gen);
 		loop
-                -- add your task code inside this loop     
+		select
+			accept stop;
+			exit;
+		else
+			Delay_Time := Duration(0.5 * Ada.Numerics.Float_Random.Random(F_Gen));
+			Value:= D_Rand.Random(D_Gen);
+			delay Delay_Time;
+			loop 
+				buffer.Producer_Turn(Producer_Flag);
+				exit when Producer_Flag;
+			end loop;
+			buffer.Insert(Value);
+		end select;
 		end loop;
 	end producer;
 
 	task body consumer is 
 		Message: constant String := "consumer executing";
-                -- add local declrations of task here 
+		Consumer_Flag: Boolean := False;
+		Sum : Integer := 0;
+        F_Gen: Ada.Numerics.Float_Random.Generator;
+		Value : PInteger;
+        Delay_Time : Duration;
 	begin
-		Put_Line(Message);
+		Ada.Numerics.Float_Random.Reset(F_Gen);
+
 		Main_Cycle:
 		loop 
-                -- add your task code inside this loop   
-		end loop Main_Cycle; 
+		Delay_Time:= Duration(1.5 * Ada.Numerics.Float_Random.Random(F_Gen));
+		delay Delay_Time;
 
-                -- add your code to stop executions of other tasks     
+		loop 
+			buffer.Consumer_Turn(Consumer_Flag);
+			exit when Consumer_Flag or buffer.Is_Stopped;
+		end loop;
+
+      exit Main_Cycle when Buffer.Is_Stopped;
+
+		loop
+			buffer.Consumer_Turn(Consumer_Flag);
+			buffer.Remove (Value);
+			Sum := Sum + Value;
+
+			-- Put_Line ("Consumed: " & Integer'Image(Value));
+			-- Put_Line ("Sum: " & Integer'Image(Sum));
+
+			if Sum > 100 then
+				Put_Line ("Consumer: Reached termination condition. Sum = " & Integer'Image(Sum));
+               	exit Main_Cycle;
+            end if;
+			
+        end loop;
+      end loop Main_Cycle;
+		
+	  producer.stop;
+	  buffer.stop;
+    
+		exception
+			  when TASKING_ERROR =>
+				  Put_Line("Buffer finished before producer");
 		Put_Line("Ending the consumer");
 	end consumer;
-
 begin
-Put_Line(Message);
+	Put_Line(Message);
 end comm2;
